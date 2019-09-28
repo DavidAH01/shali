8611ebbf3c6ed8e3ed84ab2fea3c14c4f36aa46b
David Ardila <david.ardila@uberflug.co>
2019-09-28 10:07:29 -0500

    Add home page and user module's bug fixes

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginPageComponent } from '../user/login-page/login-page.component';
-
+import { EmailLoginComponent } from '../user/email-login/email-login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginPageComponent }
+  { path: '', component: EmailLoginComponent }
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,12 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { GoogleSigninDirective } from './google-signin.directive';
 import { EmailLoginComponent } from './email-login/email-login.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [GoogleSigninDirective, EmailLoginComponent, LoginPageComponent],
+  declarations: [GoogleSigninDirective, EmailLoginComponent],
   exports: [GoogleSigninDirective],
   imports: [
     CommonModule,
