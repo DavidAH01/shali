edce03a713776c645645f306aa0d3694cd5d43ca
Cristian Arrieta <cristian.arrieta@mjv.com.br>
2019-09-28 16:54:55 -0500

    Adjust Posts

--- src/app/post-card/post-card.component.ts
+++ src/app/post-card/post-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { EventsService } from '../services/events.service';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-card',
@@ -7,13 +9,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostCardComponent implements OnInit {
   @Input() post: any;
-
-  constructor() { }
+  @Output() returnPost: any = new EventEmitter();
+  constructor(public eventService: EventsService) { }
 
   ngOnInit() {
     if (this.post.image) {
       this.post.image = JSON.parse(this.post.image)[0];
     }
   }
+  openDetail(post) {
+    this.returnPost.emit(post);
+  }
+
 
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostServiceService } from '../services/post-service.service';
+import { EventsService } from '../services/events.service';
+import { UserServiceService } from '../services/user-service.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -7,12 +9,21 @@ import { PostServiceService } from '../services/post-service.service';
 })
 export class PostComponent implements OnInit {
 
-  constructor(public postService: PostServiceService) { }
+  constructor(public postService: PostServiceService, public eventService: EventsService, public userService: UserServiceService) { }
   posts = [];
+  cardOpen: any;
+  showDetailCard = false;
   ngOnInit() {
     this.postService.getPosts().subscribe(data => {
       this.posts = data;
     });
   }
-
+  async selectedCard(card){
+    this.cardOpen = card;
+    this.showDetailCard = true;
+    await this.userService.getUser(this.cardOpen.UUID).subscribe(data => {
+      this.cardOpen.user = data;
+      console.log(this.cardOpen.user);
+    });
+  }
 }
